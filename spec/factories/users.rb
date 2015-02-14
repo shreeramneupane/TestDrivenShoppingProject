dd9695f82bca4ec3bd6e8820cbb5aaab27ff33cf
shreeramneupane <shreeramneupane@hotmail.com>
2015-02-14 19:06:00 +0545

    test case for presence of first_name last_name phone and email written

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    
+    first_name{Faker::Name.first_name}
+    middle_name{}
+    last_name{Faker::Name.last_name}
+    email{Faker::Internet.email}
+    encrypted_password{Faker::Internet.password}
+    phone{Faker::PhoneNumber.cell_phone}
+    is_admin{[true, false].sample}
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+  describe 'validate presence' do
+
+    subject { build(:user) }
+
+    it{ is_expected.to validate_presence_of(:first_name) }
+    it{ is_expected.to validate_presence_of(:last_name) }
+    it{ is_expected.to validate_presence_of(:email) }
+    it{ is_expected.to validate_presence_of(:phone) }
+
+  end
 end
